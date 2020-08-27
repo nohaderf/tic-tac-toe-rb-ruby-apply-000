093d5d79c588ca4029e6393c4f6f5a300d2d5c55
Freda Hon <freda.hon@gmail.com>
2020-08-27 00:08:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,7 +45,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index, user)
+    move(board, index, user= "X")
     display_board(board)
   else
     turn(board)
